fd14bc6f437aeaf503c7e8f6e606f6f476c87dbf
thejoshlamb <joshlamb@gmail.com>
2014-01-27 17:07:12 -0800

    styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$('.close').on('click',function(){
-		$('.main').css('bottom','-10em');
+		$('.main').css('bottom','-10em',function(){
+			this.hide();
+		});
 	});
 });
